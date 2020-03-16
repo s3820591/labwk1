8936ca44a45a7cd16b8ade9b4b21978b1de63fd8
ASUS <s3820591@rmit.edu.vn>
2020-03-16 10:36:58 +0700

    ex 7 done

--- src/Ex7.java
+++ src/Ex7.java
@@ -2,6 +2,11 @@ import java.util.Scanner;
 
 public class Ex7 {
     public static void main(String[] args) {
-
+        final double CONVERSION_RATE = 1.609;
+        System.out.println("Miles\tKilometers\t|\tKilometers\tMiles");
+        for (int mile = 1; mile <= 10; mile++) {
+            System.out.printf("%2d\t\t %5.2f\t\t|\t\t%d \t\t%.2f%n",
+                    mile, mile * CONVERSION_RATE, 15 + mile * 5, (15 + mile * 5) / CONVERSION_RATE);
+        }
     }
 }
--- src/Ex8.java
+++ src/Ex8.java
@@ -1,6 +1,6 @@
 public class Ex8 {
     public static void main(String[] args) {
-        int count = 0
+        int count = 0;
         for (int i = 100; i <= 200; i++) {
             if ((i % 6 == 0 && i % 5 !=0) || (i % 5 == 0 && 1 % 6 != 0)) {
                         System.out.print(i + " ");
