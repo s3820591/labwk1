900760863c500a1784c07e3e7ea7358bc737f6f2
ASUS <s3820591@rmit.edu.vn>
2020-07-02 10:00:21 +0700

    Ex 4 fix

--- src/Ex4.java
+++ src/Ex4.java
@@ -9,18 +9,17 @@ public class Ex4 {
         num1 = input.nextInt();
         num2 = input.nextInt();
         num3 = input.nextInt();
-        if (num1 < num2)
-            min = num1;
-        else min = num2;
-        if (num1 < num2)
-            max = num2;
-        else max = num1;
+        min = num1;
+        max = num2;
+        if (num1 > num2) {
+            min = num2 ; max = num1;
+        }
         if (num3 < min)
-            System.out.println(num3 + "," + min + "," + max);
+            System.out.println(num3 + ", " + min + ", " + max);
         else if (num3 > max)
-            System.out.println(min + "," + max + "," + num3);
+            System.out.println(min + ", " + max + ", " + num3);
         else
-            System.out.println(min + "," + num3 + "," + max);
+            System.out.println(min + ", " + num3 + ", " + max);
 
 
 
